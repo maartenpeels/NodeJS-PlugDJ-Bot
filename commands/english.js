9b5a9edef9b35e3d62407f34780570f1c29c77e2
maartenpeels <maartenpeels@gmail.com>
2017-02-15 17:12:38 +0100

    Ask user to speak english

---
+++ commands/english.js
@@ -0,0 +1,63 @@
+exports.names = ['english'];
+exports.hidden = true;
+exports.enabled = true;
+exports.cdAll = 30;
+exports.cdUser = 30;
+exports.cdStaff = 10;
+exports.minRole = PERMISSIONS.RDJ_PLUS;
+exports.handler = function (data) {
+    var params = _.rest(data.message.split(' '), 1);
+    var ch = '';
+    var lang = 'en';
+
+    if (params.length >= 1) {
+        username = params.join(' ').trim()
+        usernameFormatted = S(username).chompLeft('@').s;
+        var user = findUserInList(bot.getUsers(), usernameFormatted);
+        if (user) {
+            ch += '@' + usernameFormatted + ' ';
+            lang = user.language;
+        }
+    }
+
+    switch (lang) {
+        case 'da':
+            ch += 'Vær venlig at tale engelsk.';
+            break;
+        case 'sv':
+            ch += 'Vänligen tala engelska.';
+            break;
+        case 'de':
+            ch += 'Bitte sprechen Sie Englisch.';
+            break;
+        case 'es':
+            ch += 'Por favor, hable Inglés.';
+            break;
+        case 'fr':
+            ch += 'Parlez anglais, s\'il vous plaît.';
+            break;
+        case 'nl':
+            ch += 'Spreek Engels, alstublieft.';
+            break;
+        case 'pl':
+            ch += 'Proszę mówić po angielsku.';
+            break;
+        case 'pt':
+            ch += 'Por favor, fale Inglês.';
+            break;
+        case 'sk':
+            ch += 'Hovorte po anglicky, prosím.';
+            break;
+        case 'cs':
+            ch += 'Mluvte prosím anglicky.';
+            break;
+        case 'sr':
+            ch += 'Молим Вас, говорите енглески.';
+            break;
+        default:
+            ch += 'Speak English, please.';
+            break;
+    }
+
+	sendChat(ch);
+};
